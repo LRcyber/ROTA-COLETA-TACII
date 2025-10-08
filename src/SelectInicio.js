c8c7bf0bea35d896e1032e608acea65fe60d339e
Luciana Roncarati <52681363+LRcyber@users.noreply.github.com>
2025-10-07 21:56:47 -0300

    Update SelectInicio.js
    css update

--- src/SelectInicio.js
+++ src/SelectInicio.js
@@ -1,76 +1,39 @@
+import { Picker } from "@react-native-picker/picker";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import RNPickerSelect from "react-native-picker-select";
 
 export default function SelectInicio({ pontos, inicio, setInicio }) {
-  const placeholder = {
-    label: 'Selecione um ponto de partida...',
-    value: null,
-    color: '#9EA0A4', 
-  };
-
   return (
     <View style={styles.card}>
       <Text style={styles.label}>Ponto de Partida:</Text>
-      <RNPickerSelect
-        onValueChange={(value) => {
-          console.log('Valor selecionado:', value); 
-          setInicio(value);
-        }}
-        items={pontos.map((p) => ({ label: p, value: p, key: p }))}
-        value={inicio} 
-        placeholder={inicio ? undefined : placeholder} 
-        style={pickerSelectStyles}
-        useNativeAndroidPickerStyle={false} 
-        fixAndroidTouchableBug={true} 
-      />
+      <View style={styles.pickerWrapper}>
+        <Picker
+          selectedValue={inicio}
+          onValueChange={(itemValue) => setInicio(itemValue)}
+          mode="dropdown"
+        >
+          {pontos.map((p) => (
+            <Picker.Item label={p} value={p} key={p} />
+          ))}
+        </Picker>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    width: "100%",
-    backgroundColor: "#f9f9f9",
-    padding: 12,
-    borderRadius: 12,
+    width: "80%",
+    backgroundColor: "#bff0d9ff",
+    padding: 10,
+    borderRadius: 10,
     marginVertical: 10,
   },
-  label: { 
-    marginBottom: 6, 
-    fontWeight: "600", 
-    color: "#2f3640" 
-  },
-});
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: '#ddd',
+  label: { marginBottom: 6, fontWeight: "600", color: "#2f3640" },
+  pickerWrapper: {
+    height: 50,
     borderRadius: 8,
-    backgroundColor: 'white',
-    color: 'black',
-    paddingRight: 30, 
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 8,
-    backgroundColor: 'white',
-    color: 'black',
-    paddingRight: 30,
-  },
-  placeholder: {
-    color: '#9EA0A4', 
-    fontStyle: 'italic', 
-  },
-  viewContainer: {
-    backgroundColor: 'white',
+    overflow: "hidden",
+    backgroundColor: "#fff",
   },
 });
